4ee543f936d31eba120f78900624030103f64508
pro7onmatrix <yannick.lemke@posteo.de>
2021-02-19 16:37:41 +0100

    Update

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  /* Icon  Command                  Update Interval  Update Signal */
+  { "",    "sb-updates",            1800,            10 },
+  { "",    "sb-volume",             0,               12 },
+  { "",    "sb-kblayout",           0,               11 },
+  { "",    "sb-date",               1,               14 },
+  { "",    "sb-battery",            300,             13 },
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
