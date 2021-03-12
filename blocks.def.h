bf19802386f5a09c3fd1c18777f04c0a10613760
pro7onmatrix <yannick.lemke@posteo.de>
2021-03-12 10:11:30 +0100

    ArchLinux config

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,10 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /* Icon  Command                  Update Interval  Update Signal */
-  { "",    "sb-updates",            1800,            10 },
+  { "",    "sb-playback",           1,               10 },
+  { "",    "sb-updates",            1800,            11 },
   { "",    "sb-volume",             0,               12 },
-  { "",    "sb-kblayout",           0,               11 },
   { "",    "sb-date",               1,               14 },
-  { "",    "sb-battery",            300,             13 },
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
